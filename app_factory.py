72a69a1db4a69f7ed25c6ca08879bb55e1f3a5a2
Simon Sure <info@simonsure.com>
2023-10-21 15:42:46 +0200

    Update app_factory.py

--- app_factory.py
+++ app_factory.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from extensions import db
 from flask_cors import CORS
@@ -11,7 +13,7 @@ def create_app():
 
     # Configurations
     firebase_admin.initialize_app()
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://datagrip:ow94t8cmuo49tu8cs@34.90.69.65:5432/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')# 'postgresql://datagrip:ow94t8cmuo49tu8cs@34.90.69.65:5432/postgres'
     stripe.api_key = "sk_test_51O3HaIC7Nc96FuKDPP4x5n6mwanPUKYqVK9dZw0gxxBv2CB7jjN73USNWNXGcwoClL9tzDjyNGhkNS2YPUthJL4v00GNJAuVcJ"
 
     # Initialize extensions
